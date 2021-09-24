fe2067644565be1437fe2b86ba8b85518c0869ad
hityne <long@live.ca>
2021-09-24 16:53:43 +0800

    minor

--- filerun_docker_install.sh
+++ filerun_docker_install.sh
@@ -6,4 +6,14 @@ cd /filerun
 
 wget -O docker-compose.yml https://raw.githubusercontent.com/hityne/centos/ur/docker-compose.yml
 
+
+filerun_root_passwd=cat /proc/sys/kernel/random/uuid|cut -f1 -d"-"
+filerun_user_passwd=cat /proc/sys/kernel/random/uuid|cut -f1 -d"-"
+
+sed -i "s/filerun_root_passwd/$filerun_root_passwd/" docker-compose.yml
+sed -i "s/filerun_used-passwd/$filerun_user_passwd/g" docker-compose.yml
+echo ""
+echo "please check /filerun/docker-compose.yml to get more configurations."
+echo ""
 docker-compose up -d
+
