ce36392a6bcaa4368b16222147ec3d37db7dac81
hityne <long@live.ca>
2022-04-08 00:02:55 +0800

    22

--- mydebian.sh
+++ mydebian.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 更新和升级
-apt update && apt dist-upgrade
+apt update && apt dist-upgrade -y
 
 # 安装必要包
 apt install -y git wget curl vim
@@ -10,5 +10,4 @@ apt install -y git wget curl vim
 wget https://raw.githubusercontent.com/hityne/others/main/vimrc.local && mv vimrc.local /etc/vim/
 
 # ssh控制台添加颜色
-sed -i "$a\alias ls='ls --color=auto'" /etc/profile
-
+sed -i "\$a\alias ls='ls --color=auto'" /etc/profile
