4ee8a1dd70d985bf89d3d12ae48e75291369cbc0
Richard <accdou@163.com>
2022-04-08 13:54:16 +0800

    minor

--- ifexist.sh
+++ ifexist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 which python >/dev/null 2>&1
-echo $?
-if [ $? === 0 ]; then
-    echo "没有此命令！"
+echo $?;
+if [ $? == 0 ]; then
+    echo "有此命令！"
 else
-    echo "有此命令。"
+    echo "没有此命令。"
 fi
