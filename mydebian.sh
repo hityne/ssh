ee0c1c6d79a4154b1292afe0078d75ab18bcb8e2
hityne <long@live.ca>
2022-04-17 23:56:52 +0800

    ..

--- mydebian.sh
+++ mydebian.sh
@@ -11,3 +11,6 @@ wget https://raw.githubusercontent.com/hityne/others/main/vimrc.local && mv vimr
 
 # ssh控制台添加颜色
 sed -i "\$a\alias ls='ls --color=auto'" /etc/profile
+
+# 降低IPv6优先级，有限使用IPv4
+echo "precedence ::ffff:0:0/96 100" >>/etc/gai.conf
