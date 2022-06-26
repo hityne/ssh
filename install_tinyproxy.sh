138b5364625b23edc3f8d1076e1553d445130d30
hityne <accdou@163.com>
2022-06-27 07:14:50 +0800

    ..

--- install_tinyproxy.sh
+++ install_tinyproxy.sh
@@ -9,7 +9,7 @@ echo ""
 echo "Tinyproxy has been installed."
 echo "#Allow 127.0.0.1 ----> Allow 127.0.0.1"                                                     #"
 read -p "Please input your port:" myport
-sed -i "s/Port 8888/Port $myport/" tinyproxy.conf
+sed -i "s/Port 8888/Port $myport/" /etc/tinyproxy/tinyproxy.conf
 echo "Port 8888 ----> Port $myport"    
 ps -ef | grep tinyproxy | grep -v grep| awk '{print "kill -9 "$2}' | sh
 service tinyproxy start
