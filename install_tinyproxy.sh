53033fa1f11a1bd286950d73d84e7e3b7ab51bd2
Richard <accdou@163.com>
2022-06-27 06:38:38 +0800

    add tinyproxy

---
+++ install_tinyproxy.sh
@@ -0,0 +1,16 @@
+apt update
+apt install tinyproxy
+
+
+sed -i "s/Allow 127.0.0.1/#Allow 127.0.0.1/" tinyproxy.conf
+
+clear
+echo ""
+echo "Tinyproxy has been installed."
+echo "#Allow 127.0.0.1 ----> Allow 127.0.0.1"                                                     #"
+read -p "Please input your port:" myport
+sed -i "s/Port 8888/Port $myport/" tinyproxy.conf
+echo "Port 8888 ----> Port $myport"    
+ps -ef | grep tinyproxy | grep -v grep| awk '{print "kill -9 "$2}' | sh
+service tinyproxy start
+service tinyproxy status
