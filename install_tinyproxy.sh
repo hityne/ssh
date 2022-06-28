2af2625999757a412f8341c0267c254457dc89c0
hityne <accdou@163.com>
2022-06-28 10:54:34 +0800

    tinyproxy

--- install_tinyproxy.sh
+++ install_tinyproxy.sh
@@ -1,16 +1,44 @@
-apt update
-apt install tinyproxy
 
 
-sed -i "s/Allow 127.0.0.1/#Allow 127.0.0.1/" /etc/tinyproxy/tinyproxy.conf
 
 clear
 echo ""
-echo "Tinyproxy has been installed."
-echo "#Allow 127.0.0.1 ----> Allow 127.0.0.1"                                                     #"
-read -p "Please input your port:" myport
-sed -i "s/Port 8888/Port $myport/" /etc/tinyproxy/tinyproxy.conf
-echo "Port 8888 ----> Port $myport"    
-ps -ef | grep tinyproxy | grep -v grep| awk '{print "kill -9 "$2}' | sh
-service tinyproxy start
-service tinyproxy status
+
+#read -s -n1 -p "Press any key to continue..." 
+
+echo "  1.Install tinyproxy                           2.Uninstall tinyproxy"
+echo "  3.reset tinyproxy                             4.Modify configuire"
+
+
+echo ""
+read -p "Please input the number you choose:" main_no
+
+if [ "$main_no" = "1" ]; then
+    apt update
+    apt install -y tinyproxy
+    sed -i "s/Allow 127.0.0.1/#Allow 127.0.0.1/" /etc/tinyproxy/tinyproxy.conf
+      
+    clear
+    echo ""
+    echo "Tinyproxy has been installed."
+    echo "#Allow 127.0.0.1 ----> Allow 127.0.0.1"    
+                                               #"
+    read -p "Please input your port:" myport
+    sed -i "s/Port 8888/Port $myport/" /etc/tinyproxy/tinyproxy.conf
+    echo "Port 8888 ----> Port $myport"  
+    echo ""  
+    service tinyproxy restart
+    service tinyproxy status
+elif [ "$main_no" = "2" ]; then
+    apt remove -y tinyproxy
+
+elif [ "$main_no" = "3" ]; then
+    ps -ef | grep tinyproxy | grep -v grep| awk '{print "kill -9 "$2}' | sh
+    service tinyproxy start
+    service tinyproxy status
+elif [ "$main_no" = "4" ]; then
+    vim etc/tinyproxy/tinyproxy.conf
+else
+    exit 0
+fi
+
