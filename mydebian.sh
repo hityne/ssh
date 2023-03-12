8a7fd38dd34205fd1b98dfeec5828fa8a4a14969
Richard <89051023+hityne@users.noreply.github.com>
2023-03-12 20:46:16 +0800

    Update mydebian.sh

--- mydebian.sh
+++ mydebian.sh
@@ -6,15 +6,9 @@ apt update && apt dist-upgrade -y
 # 安装必要包
 apt install -y git wget curl vim screen ufw ntp ntpdate
 
-# 降低IPv6优先级，有限使用IPv4
-res=$(sed -n '/^precedence.*100$/p' /etc/gai.conf)
-if [ -z "$res" ]; then 
-    read -p "Do you want to prior to use IPv4 instead of IPv6? [n] no (defaut) [y] yes: " if_option
-	if [ "$if_option" = "y" ]; then
-        echo "precedence ::ffff:0:0/96 100" >>/etc/gai.conf
-    fi
-
-fi
+#修改系统语言环境
+echo 'LANG="en_US.UTF-8"' >> /etc/profile
+source /etc/profile
 
 # vim右键复制粘贴
 wget https://raw.githubusercontent.com/hityne/others/main/vimrc.local && mv vimrc.local /etc/vim/
@@ -22,3 +16,14 @@ wget https://raw.githubusercontent.com/hityne/others/main/vimrc.local && mv vimr
 # ssh控制台添加颜色
 sed -i "\$a\alias ls='ls --color=auto'" /etc/profile
 
+# 降低IPv6优先级，优先使用IPv4
+read -p "Do you want to modifiy the priority of  IPv4=6 [n] no (defaut) [y] yes: " if_mod
+if [ "$if_mod" = "y" ]; then
+        res=$(sed -n '/^precedence.*100$/p' /etc/gai.conf)
+	if [ -z "$res" ]; then 
+	    read -p "Do you want to prior to use IPv4 instead of IPv6? [n] no (defaut) [y] yes: " if_option
+	    if [ "$if_option" = "y" ]; then
+		echo "precedence ::ffff:0:0/96 100" >>/etc/gai.conf
+	    fi
+	fi
+fi
