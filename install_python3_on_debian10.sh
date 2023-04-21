81a03946cb8821b9295a57b839c9a9fcda20f76f
Richard <89051023+hityne@users.noreply.github.com>
2023-04-21 09:14:12 +0800

    Update install_python3_on_debian10.sh

--- install_python3_on_debian10.sh
+++ install_python3_on_debian10.sh
@@ -3,17 +3,17 @@
 [[ $(id -u) != 0 ]] && echo -e " 请使用root用户运行此脚本！" && exit 1
 
 echo ""
-echo "This script will install python3.9.12 and virtualenv on Debian 10/11."
+echo "This script will install python3.9.16 and virtualenv on Debian 10/11."
 echo ""
 read -s -n1 -p "Press any key to continue..." 
 
 apt update
 apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
 
-wget https://www.python.org/ftp/python/3.9.12/Python-3.9.12.tgz
-tar -xf Python-3.9.12.tgz
+wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
+tar -xf Python-3.9.16.tgz
 
-cd Python-3.9.12
+cd Python-3.9.16
 ./configure --enable-optimizations
 
 #编译安装
@@ -27,8 +27,8 @@ if [ "$py_option" -ne "1" ]; then
     make altinstall
 
     cd ..
-    rm -rf Python-3.9.12
-    rm -f Python-3.9.12.tgz
+    rm -rf Python-3.9.16
+    rm -f Python-3.9.16.tgz
 
     echo ""
     python3.9 --version
@@ -50,8 +50,8 @@ else
     make install
 
     cd ..
-    rm -rf Python-3.9.12
-    rm -f Python-3.9.12.tgz
+    rm -rf Python-3.9.16
+    rm -f Python-3.9.16.tgz
 
     echo ""
     python3 --version
