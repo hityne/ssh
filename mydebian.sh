7bddffabe0529543c9576627481347ea5d030e66
Richard <89051023+hityne@users.noreply.github.com>
2023-05-07 09:29:01 +0800

    Update mydebian.sh

--- mydebian.sh
+++ mydebian.sh
@@ -6,6 +6,9 @@ apt update && apt dist-upgrade -y
 # 安装必要包
 apt install -y git wget curl vim screen ufw ntp ntpdate
 
+# 安装语言包
+dpkg-reconfigure locales
+
 #修改系统语言环境
 echo 'LANG="en_US.UTF-8"' >> /etc/profile
 source /etc/profile
@@ -18,13 +21,13 @@ sed -i "\$a\alias ls='ls --color=auto'" /etc/profile
 source /etc/profile
 
 # 降低IPv6优先级，优先使用IPv4
-read -p "Do you want to modifiy the priority of  IPv6 [n] no (defaut) [y] yes: " if_mod
-if [ "$if_mod" = "y" ]; then
-        res=$(sed -n '/^precedence.*100$/p' /etc/gai.conf)
-	if [ -z "$res" ]; then 
-	    read -p "Do you want to prior to use IPv4 instead of IPv6? [n] no (defaut) [y] yes: " if_option
-	    if [ "$if_option" = "y" ]; then
-		echo "precedence ::ffff:0:0/96 100" >>/etc/gai.conf
-	    fi
-	fi
-fi
+# read -p "Do you want to modifiy the priority of  IPv6 [n] no (defaut) [y] yes: " if_mod
+# if [ "$if_mod" = "y" ]; then
+#         res=$(sed -n '/^precedence.*100$/p' /etc/gai.conf)
+# 	if [ -z "$res" ]; then 
+# 	    read -p "Do you want to prior to use IPv4 instead of IPv6? [n] no (defaut) [y] yes: " if_option
+# 	    if [ "$if_option" = "y" ]; then
+# 		echo "precedence ::ffff:0:0/96 100" >>/etc/gai.conf
+# 	    fi
+# 	fi
+# fi
