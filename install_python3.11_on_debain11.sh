0ebad465c68a6cf2bf9601ffcbceaaf16d29045f
Richard <89051023+hityne@users.noreply.github.com>
2023-07-01 15:32:22 +0800

    Update install_python3.11_on_debain11.sh

--- install_python3.11_on_debain11.sh
+++ install_python3.11_on_debain11.sh
@@ -14,7 +14,7 @@ wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
 tar -xf Python-3.11.3.tgz
 
 cd Python-3.11.3
-./configure --enable-optimizations
+./configure --enable-optimizations --with-ssl
 
 #编译安装
 make -j $(nproc)
